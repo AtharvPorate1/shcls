1a1a0118402a37c938fec96b67cb3b092b7f4a6e
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-02-11 00:08:26 +0530

    problem

---
+++ app/(main)/diagnosis/page.tsx
@@ -0,0 +1,30 @@
+import Navbar from "@/components/Navbar";
+import Searchbar from "@/components/Searchbar";
+import { getDiagnosis } from "@/utils/ai";
+
+const getDiagnosisObj = async (message: string) => {
+    const response = await getDiagnosis(message);
+    const responseObj = JSON.parse(response?.toString() || "{}");
+    return responseObj;
+}
+
+
+
+
+const page = async() => {
+    const responseObj = await getDiagnosisObj("I have a headache");
+ 
+    // const doctor_needed = responseObj["doctor_needed"];
+    // const diagnosis = responseObj["diagnosis"];
+    // console.log(responseObj.doctor_needed);
+
+    return (
+        <div>
+            <Navbar/>
+            <Searchbar/>
+            {/* {response} */}
+        </div>
+    );
+}
+
+export default page;
